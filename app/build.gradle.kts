28dd6a618fc2378ab6b3ad4914ab1af6cf3ac39a
unknown <thelinh2711@gmail.com>
2025-03-29 22:52:05 +0700

    complete NoteDatabase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.room.common)
+    implementation(libs.androidx.room.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/example/appnote/data/NoteDatabase.kt
@@ -0,0 +1,34 @@
+package com.example.appnote.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [Note ::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class NoteDatabase:RoomDatabase() {
+
+    // Khai báo DAO để Room tự tạo implementation
+    abstract fun noteDao():NoteDAO
+
+    companion object {
+        @Volatile
+        private var INSTANCE: NoteDatabase? = null
+
+        fun getInstance(context: Context): Any {
+            return INSTANCE ?: synchronized(this){
+                var instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    NoteDatabase::class.java,
+                    "note_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
