d503bff14ddf2fbe3a4c99227faceb08c3a887b4
unknown <thelinh2711@gmail.com>
2025-03-29 21:50:10 +0700

    complete NoteDAO

--- app/src/main/java/com/example/appnote/data/NoteDAO.kt
+++ app/src/main/java/com/example/appnote/data/NoteDAO.kt
@@ -1,4 +1,21 @@
 package com.example.appnote.data
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
 interface NoteDAO {
+    @Insert
+    fun insert(note: Note)
+
+    @Delete
+    fun delete(note: Note)
+
+    @Query("SELECT * FROM notes ORDER BY created_time DESC")
+    fun getAllNotes() : List<Note>
+
+    @Query("SELECT * FROM notes WHERE note_title LIKE:keyword ORDER BY created_time DESC")
+    fun searchNotes(keyword : String) : List<Note>
 }
--- app/src/main/java/com/example/appnote/ui/AddEditNoteActivity.kt
+++ app/src/main/java/com/example/appnote/ui/AddEditNoteActivity.kt
@@ -1,6 +1,8 @@
 package com.example.appnote.ui
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,8 +10,19 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.appnote.R
 
 class AddEditNoteActivity : AppCompatActivity() {
+    private lateinit var etTitle: EditText
+    private lateinit var etContent: EditText
+    private lateinit var btnSave: ImageButton
+    private lateinit var btnBack: ImageButton
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_edit_note)
+        initView();
+    }
+
+    private fun initView() {
+        
     }
 }
